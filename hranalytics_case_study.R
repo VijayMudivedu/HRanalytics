9fdc120adefb90a8dcce83d12e7be95de0f2f2d3
Vijay Mudivedu <Vijay.Mudivedu@iiitb.net>
2018-03-06 07:05:40 +0530

    small changes added to the code around the test duration calculation

--- hranalytics_case_study.R
+++ hranalytics_case_study.R
@@ -43,61 +43,104 @@
 # You are supposed to code entirely in R. All your plots and tables must be created in R, though you may recreate the same in Tableau as well (for the presentation) for better aesthetics. Please submit the presentation in a PDF format. Please make sure to rename your R script as "Group_Facilitator_RollNo_main.R".
 
 
+# READ ALL EXCEL FILES
+
+
 # read HR analytics files
 employee_survey_data <- read.csv("PA-I_Case_Study_HR_Analytics/employee_survey_data.csv")
 general_data <- read.csv("PA-I_Case_Study_HR_Analytics/general_data.csv")
-in_time_data <- read.csv("PA-I_Case_Study_HR_Analytics/in_time.csv",stringsAsFactors = F)
+in_time_data <- read.csv("PA-I_Case_Study_HR_Analytics/in_time.csv",stringsAsFactors = F,check.names = F)
 manager_survey_data <- read.csv("PA-I_Case_Study_HR_Analytics/manager_survey_data.csv")
 out_time <- read.csv("PA-I_Case_Study_HR_Analytics/out_time.csv",stringsAsFactors = F,check.names = F)
 
-#install.packages("xlsx")
-data_dictionary <- xlsx::read.xlsx(file = "PA-I_Case_Study_HR_Analytics/data_dictionary.xlsx")
 
-hr_data_dictionary <- xlsx::read.xlsx(file = "PA-I_Case_Study_HR_Analytics/data_dictionary.xlsx",sheetName = "data_dictionary",as.data.frame = TRUE)
-View(hr_data_dictionary)
 
+data_dictionary <- readxl::read_xlsx(path = "PA-I_Case_Study_HR_Analytics/data_dictionary.xlsx")#(file = "PA-I_Case_Study_HR_Analytics/data_dictionary.xlsx")
+
+#hr_data_dictionary <- xlsx::read.xlsx(file = "PA-I_Case_Study_HR_Analytics/data_dictionary.xlsx",sheetName = "data_dictionary",as.data.frame = TRUE)
+View(data_dictionary)
+
+
+##################################
 # DATA PREPARATION
+##################################
+
 # checking for duplicates
-
-
 
 summary(employee_survey_data)
 
 # applying the str
 # checking the characterisitics of the data frame
 sapply(list(general_data,in_time_data,manager_survey_data,out_time,employee_survey_data),str)
-sapply(list(test_in,test_out),str)
 
+# CHECKING DUPLICATES
 sum(duplicated(x = employee_survey_data$EmployeeID))
+
+# COMMENTS: THERE ARE NO DUPLCIATES
 
 # substracting in and outtimes of the employess of two dataframes.
 
 library(lubridate)
 
+# IN_TIME AND OUT_TIME DATA_FRAME ANALYSIS
+
+# GOAL OF THIS ANALYSIS IS TO FIND THE REGULARITY OF AN EMPLOYEE, VACATIONS TAKEN BY THE EMPLOYEE
+
+# MISSING DATA IS ASSUMED AS "VACATION/TIME_OFF"
+
+# LET US CREATE A SUBSET DATAFRAMES WITH LIMITED DATA TO TEST THE DATE TIME DIFFERENCE
+
+# ********************************************************
+# USING THE TEST DATAFRAMNES
+# ********************************************************
+
 # test date difference between mulitple columns
 test_in <- in_time_data[1:20,1:5]
 
 test_out <- out_time[1:20,1:5]
-str(test_in)
-str(test_out)
+#str(test_in)
+#str(test_out)
 
-#View(test_in)
-#?lubridate
-# converting the data to parse_date_time format
-x12 <- parse_date_time(test_in$`2015-01-05`, orders = "Y-m-d H:M:S",tz = "GMT")
+# replace NAs with "time_off" text
+which(is.na(test_out))
+test_in[is.na(test_in)] <- "time_off"
+test_out[is.na(test_out)] <- "time_off"
+
+
+# converting the data to parse_date_time format one column
+x12 <- parse_date_time(test_in$`2015-01-02`, orders = "Y-m-d H:M:S",tz = "GMT")
 x13 <- parse_date_time(test_out$`2015-01-02`,orders = "Y-m-d H:M:S",tz = "GMT")
 x12
 x13
 
+# trying to convert the complete dataframe in parse_date_time converting
+# parse_date_time(test_in,orders = "Y-m-d H:M:S")
+# doesn't seam to work
+
+
 #format(as.Date(test_in$`2015-01-02`[1:5],format = "%Y-%m-%d"),"%Y")
 
-#diff.Date(test_in$X2015.01.01)[1:5]
-difftime(time1 = x12,time2 = x13)
+test_dur <- matrix(data = 0,nrow = nrow(test_in), ncol = ncol(test_in))
 
-#
-which(is.na(test_out))
-test_in[is.na(test_in)] <- "time_off"
-test_out[is.na(test_out)] <- "time_off"
+
+for(i in 1:length(test_in)) {
+
+test_dur[,i] <- difftime(time1 = parse_date_time(test_out[,i],orders = "Y-m-d H:M:S"),
+         time2 = parse_date_time(test_in[,i],orders = "Y-m-d H:M:S"), 
+         units = "hours")
+      }
+
+colnames(test_dur) <- c(names(test_in))
+
+test_dur[is.na(test_dur)] <- "time_off"
+
+test_dur <- test_dur[,-1]
+test_dur <- data.frame(test_dur)
+round(test_dur,2)
+
+
+#difftime(time1 = x13,time2 = x12,units = "hours")
+
 
 # difftime(time1 = parse_date_time(test_in[,-c(1,2)],orders = "Y-m-d H:M:S"),
 #         time2 = parse_date_time(test_out[,-c(1,2)],orders = "Y-m-d H:M:S"),tz = "GMT",units = c("auto"))
@@ -114,6 +157,21 @@ test_time_in_time_out <- rbind(test_in,test_out)
 test_time_in_time_out[1:20,] - test_time_in_time_out[21:40,]
 
 ?diff.difftime()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #
